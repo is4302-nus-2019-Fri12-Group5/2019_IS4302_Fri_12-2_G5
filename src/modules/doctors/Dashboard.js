c20c49ecdf0903b24068bbc820f60f72345fccd1
April0616 <594395096@qq.com>
2019-04-05 15:59:24 +0800

    update doctor login path

--- src/modules/doctors/Dashboard.js
+++ src/modules/doctors/Dashboard.js
@@ -6,10 +6,18 @@ import { Helmet } from 'react-helmet'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
-import { grey3 } from '../../ui/common/colors'
+import {grey2, grey3} from '../../ui/common/colors'
 
 // App Imports
 import DoctorMenu from './common/Menu'
+import ImageTile from "../../ui/image/Tile";
+import Button from '../../ui/button'
+import {level1} from "../../ui/common/shadows";
+import {APP_URL} from "../../setup/config/env";
+import {H3, H4} from "../../ui/typography";
+import {Link} from "react-router-dom";
+import doctorsRoutes from "../../setup/routes/doctors";
+import home from "../../setup/routes/home";
 
 // Component
 const Dashboard = () => (
@@ -23,11 +31,54 @@ const Dashboard = () => (
     <DoctorMenu/>
 
     {/* Page Content */}
-    <Grid style={{ padding: '2em' }}>
-      <GridCell>
-        <p style={{ textAlign: 'center', color: grey3 }}>Nothing here yet. Hello World.</p>
-      </GridCell>
-    </Grid>
+    {/*<Grid style={{ padding: '2em' }}>*/}
+      {/*<GridCell>*/}
+        {/*<p style={{ textAlign: 'center', color: grey3 }}>Nothing here yet. Hello World.</p>*/}
+      {/*</GridCell>*/}
+    {/*</Grid>*/}
+
+      <Grid gutter={true} alignCenter={true} style={{ padding: '2em' }}>
+
+          {/* SEO */}
+          <Helmet>
+              <title>My Profile - MediChain</title>
+          </Helmet>
+
+          {/* Left Content - Image Collage */}
+          <GridCell>
+              <Grid gutter={true} alignCenter={true}>
+                  <GridCell justifyCenter={true}>
+                      <ImageTile width={700} height={630} shadow={level1} image={`${ APP_URL }/images/doctor_1.jpg`}/>
+                  </GridCell>
+              </Grid>
+          </GridCell>
+
+          <GridCell style={{textAlign: 'center' }}>
+              <H3 font="secondary" style={{ marginBottom: '1em' }}>My Profile</H3>
+              {/*<H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>*/}
+              <H4 style={{ marginBottom: '3em' }}>Bruce Lee</H4>
+              {/*<p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>*/}
+              <p style={{ color: grey2, marginBottom: '2em' }}>License:   AX000123</p>
+              <p style={{ color: grey2, marginBottom: '2em' }}>Department:   Orthopedics</p>
+              <p style={{ color: grey2, marginBottom: '2em' }}>Title: Director</p>
+              <p style={{ color: grey2, marginBottom: '4em' }}>Fee:   100</p>
+
+              <Link to={doctorsRoutes.dashboard.path}>
+                  <Button theme="primary" style={{marginRight : '0.5em'}}>Dashboard</Button>
+              </Link>
+
+              {/*<Link to={}>*/}
+              <Button type="button" style={{marginRight : '0.5em'}}>Edit</Button>
+              {/*</Link>*/}
+
+              {/*<Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>*/}
+              <Link to={home.home.path}>
+                  <Button type="button" theme="secondary" >Logout</Button>
+              </Link>
+          </GridCell>
+
+      </Grid>
+
   </div>
 )
 
