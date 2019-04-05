3107fe28987955d874b5da2cf39d04e63ddef248
April0616 <594395096@qq.com>
2019-04-05 10:23:14 +0800

    update doctor profile

--- src/modules/doctor/Profile.js
+++ src/modules/doctor/Profile.js
@@ -15,41 +15,52 @@ import { grey, grey2 } from '../../ui/common/colors'
 import doctorRoutes from '../../setup/routes/doctor'
 import home from '../../setup/routes/home'
 import { logout } from './api/actions'
+import ImageTile from "../../ui/image/Tile";
+import {level1} from "../../ui/common/shadows";
+import {APP_URL} from "../../setup/config/env";
 
 // Component
 const Profile = (props) => (
-  <div>
-    {/* SEO */}
-    <Helmet>
-      <title>My Profile - MediChain</title>
-    </Helmet>
+    <Grid gutter={true} alignCenter={true} style={{ padding: '2em' }}>
 
-    {/* Top title bar */}
-    <Grid style={{ backgroundColor: grey }}>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H3 font="secondary">My profile</H3>
-      </GridCell>
-    </Grid>
+        {/* SEO */}
+        <Helmet>
+          <title>My Profile - MediChain</title>
+        </Helmet>
 
-    <Grid>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+        {/* Left Content - Image Collage */}
+        <GridCell>
+            <Grid gutter={true} alignCenter={true}>
+                <GridCell justifyCenter={true}>
+                    <ImageTile width={700} height={630} shadow={level1} image={`${ APP_URL }/images/doctor_1.jpg`}/>
+                </GridCell>
+            </Grid>
+        </GridCell>
+
+      <GridCell style={{textAlign: 'center' }}><H3 font="secondary" style={{ marginBottom: '1em' }}>My Profile</H3>
         {/*<H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>*/}
-          <H4 style={{ marginBottom: '0.5em' }}>Name</H4>
+          <H4 style={{ marginBottom: '3em' }}>Bruce Lee</H4>
         {/*<p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>*/}
-        <p style={{ color: grey2, marginBottom: '2em' }}>Email</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>License:   AX000123</p>
+          <p style={{ color: grey2, marginBottom: '2em' }}>Department:   Orthopedics</p>
+          <p style={{ color: grey2, marginBottom: '2em' }}>Title: Director</p>
+          <p style={{ color: grey2, marginBottom: '4em' }}>Fee:   100</p>
 
         <Link to={doctorRoutes.doctorSubscriptions.path}>
-          <Button theme="primary" style={{marginRight : '0.5em'}}>Subscriptions</Button>
+          <Button theme="primary" style={{marginRight : '0.5em'}}>Patients</Button>
         </Link>
+
+          {/*<Link to={}>*/}
+              <Button type="button" style={{marginRight : '0.5em'}}>Edit</Button>
+          {/*</Link>*/}
 
         {/*<Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>*/}
         <Link to={home.home.path}>
             <Button type="button" theme="secondary" >Logout</Button>
         </Link>
-
       </GridCell>
     </Grid>
-  </div>
+
 )
 
 // Component Properties
